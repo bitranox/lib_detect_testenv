78104341f412bcef155d94cc904246c5a3324e21
bitranox <rnowotny1966@gmail.com>
2023-07-12 00:17:05 +0200

    push coverage

--- lib_detect_testenv/lib_detect_testenv.py
+++ lib_detect_testenv/lib_detect_testenv.py
@@ -102,6 +102,10 @@ def is_pytest_active(arg_string: Optional[str] = None) -> bool:
     ----------
     none
 
+    >>> assert False == is_pytest_active(arg_string='')
+    >>> assert True == is_pytest_active(arg_string='pytest.py')
+    >>> assert True == is_pytest_active(arg_string='/pytest/__main__.py')
+
     """
     # is_pytest_active}}}
 
